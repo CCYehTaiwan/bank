650eff08bbf017a57789bf32f23b4a165618b5ee
Yeh Ching Chun <yehchingchun@Yehs-MacBook-Air.local>
2022-08-30 08:23:22 +0800

    add information to README.md

--- main.py
+++ main.py
@@ -13,10 +13,3 @@ for area in ["1", "3"]:
     _, info = house_information.search(filter_params={"region":area, "kind":'0'})
     mycol.insert_many(info)
 
-# mycol.delete_many({"city": "3"})
-"""
-if __name__ == "__main__":
-    house = House()
-    _, _, information = house.search()
-    print(information)
-"""
